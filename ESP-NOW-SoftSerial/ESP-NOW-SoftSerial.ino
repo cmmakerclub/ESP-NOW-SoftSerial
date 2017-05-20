186f068334a7039fa4bf0893e29860f490a9ab73
Nat <nat@cmmc.io>
2017-05-20 17:11:55 +0700

    Initial commit.

---
+++ ESP-NOW-SoftSerial/ESP-NOW-SoftSerial.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+
+#define rxPin 14
+#define txPin 12
+
+//SoftwareSerial mySerial (rxPin, txPin);
+SoftwareSerial swSerial(rxPin, txPin);
+
+void setup() {
+  WiFi.disconnect();
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  Serial.begin(115200);
+  swSerial.begin(9600);
+  Serial.println("\nSoftware serial slave");
+}
+
+
+void loop() {
+}
